53e4eb0fb0bbf48f7a80acaa1b073526a5d3c7cf
Nathan Pickard <nathan.pickard87@gmail.com>
2022-11-27 19:37:22 -0800

    Configuring predictions backend

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,19 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "angularamplifypredic8b164903": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "predictions": {
+        "identifyLabelse6cba163": {
+            "region": "string",
+            "type": "string"
+        }
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,15 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import { Amplify } from 'aws-amplify';
+
+import { Predictions, AmazonAIPredictionsProvider } from '@aws-amplify/predictions';
+
+import awsconfig from './aws-exports';
+
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+Amplify.configure(awsconfig);
+Amplify.addPluggable(new AmazonAIPredictionsProvider());
